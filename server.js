e671e49a1cd246457a2ba6bb2cc1d5d926cd0a80
Guilherme Bezerra da silva <137366030+GuilhermeMage@users.noreply.github.com>
2026-05-25 12:26:43 -0300

    Bot Sprint 6

---
+++ server.js
@@ -0,0 +1,59 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+const EVO_URL = process.env.EVO_URL;
+const INSTANCE_API_KEY = process.env.INSTANCE_API_KEY;
+const INSTANCE_NAME = process.env.INSTANCE_NAME;
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req,res) =>{
+    res.send("Teste bot rodando")
+});
+
+app.post("Webhook", async (req,res) =>{
+    try {
+        console.log("Webhook recebido: ");
+        console.log(JSON.stringify(req.body, null, 2));
+
+        const body = req.body;
+        const evento = body.event;
+
+        if (data.event === 'messages.upsert') {
+            const msg = data.data[0];
+            const numero = msg.key.remoteJid;
+            const texto = msg.message?.conversation || '';   
+
+
+        if (texto.toLowerCase().trim() === 'oi') {
+            await enviarResposta(numero, 'Olá, Sou o bot da Reobote!');
+        }
+        }  
+        res.status(200).send("OK!");
+    } catch (error) {
+        console.log("Erro no Webhook: ", error);
+    }
+});
+
+async function enviarResposta(numero, texto) {
+    const numeroLimpo = numero.replaca("@s.whatsapp.net,", "");
+
+     const resposta = await fetch(`${EVO_URL}/message/sendText/${INSTANCE}`, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            apikey: EVO_KEY
+        },
+    })
+        body:json.stringify({
+            number: numeroLimpo,
+            text: texto
+        })
+};
+
+app.listen(process.env.PORT || 3000);
+console.log(`Servidor rodando na porta ${PORT}`);
